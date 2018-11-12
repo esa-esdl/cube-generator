118d1fc478bc150b395686d43830bc46ebb1e33f
helge <helge.dzierzon@plantandfood.co.nz>
2018-11-12 19:07:13 +0100

    Added variable bsub commits to cube-gen.sh

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # Earth System Data Cube Version 1.0.2_2 (ESDCv1.0.2_2)
 
-This repository contains the scripts, configurations as well as providers used for individual Earth System Data Cube (ESDC) productions. The repository also contains md5 sums of the released data. 
+This repository contains the scripts, configurations as well as providers used for individual Earth System Data Cube (ESDC) production of version 1.0.2_2. 
+The repository also contains md5 sums of the released data.
+
+The version 1.0.2_2 comtains compared to version 1.0.2_1 of the following
+additional variables:
+
+- 1
+
+The following variables were updated:
+
+- 1
+
+Version 1.0.2_2 uses the zarr format. Old formatted cubes can be found in
+the attic of teh ESDC platform. 
 
 ## Cube generation
 
-### Install Pre-requisites for cube generation
+### Installing Pre-requisites for the Cube Generation
 
 * Miniconda
   * Install [Miniconda](https://conda.io/miniconda.html)
@@ -19,17 +32,48 @@ This repository contains the scripts, configurations as well as providers used f
   * `python setup.py install`
   * Run `cuge-gen` to test whether the software is correctly installed
 
-### Generation
+### Providers
 
 The esdl core library contains data providers already. However, for this version we developed new
-providers for new and updated variables. They are stored in teh sub directory `cube/providers`. In order
+providers for new and updated variables. They are stored in the sub-directory `cube/providers`. In order
 to activate them, you have to run `python setup.py` from the cube-generator root directory. 
 
 ### Adding and updating Variables for v1.0.2_2
 
 1. `cd [cube-generator]/scripts`
-3. Run the following:
+2. rsync down cube
+3. check md5sums
+4. Run the following:
    * **Windows**: `cube-gen.bat`
    * **Linux**: `./cube-gen.sh`
+5. Copy new variables to cube
+6. Generate md5sums of new variable
+7. rsync cube up
+8. commit new md5sums to repo
 
-**NOTE**: Use the following naming convention for the cube directory name: https://github.com/esa-esdl/cube-generator/wiki/Cube-file-structure-and-naming-convention
+The cube version 1.0.2_2 uses the zarr data format. Zarr serialises chunked
+data and, thus, is comprised of a large number of small files for each variable.
+For the generation we have, therefore, used the following bash commands for each 
+variable:
+
+```
+for v in $(ls -d [cube])
+do
+    find -f file [cube]/$v > [cube]/$v.md 
+done
+```
+
+Checking the md5sum will need
+
+```
+for v in $(ls -d [cube])
+do
+    find -f file [cube]/$v > [cube]/$v.md
+done
+```
+
+## Naming convention
+
+The following naming convention for the cube directories has been used: https://github.com/esa-esdl/cube-generator/wiki/Cube-file-structure-and-naming-convention
+
+
--- scripts/cube-gen.sh
+++ scripts/cube-gen.sh
@@ -7,7 +7,30 @@ cube_config=$4
 
 START_TIME=$(date)
 
-echo cube-gen "$cube_path" "$cube_provider:dir=$source_dir" -c ${cube_config}
+bsub -W 12:00 -M 64000 -e ozone_h.err -o ozone_h.out -J ozone_h cube-gen esdc/ozone/esdc-8d-0.25deg-1x720x1440-1.0.2 "ozone:dir=ozone" -c cube-low-res.config
+bsub -W 12:00 -M 64000 -e ozone_l.err -o ozone_l.out -J ozone_l cube-gen esdc/ozone/esdc-8d-0.083deg-1x2160x4320-1.0.2 "ozone:dir=ozone" -c cube-high-res.config
+bsub -W 12:00 -M 64000 -e aerosol_l.err -o aerosol_l.out -J aerosol_l cube-gen esdc/aerosol/esdc-8d-0.25deg-1x720x1440-1.0.2 "aerosols:dir=aerosol" -c cube-low-res.config
+bsub -W 12:00 -M 64000 -e aerosol_h.err -o aerosol_h.out -J aerosol_h cube-gen esdc/aerosol/esdc-8d-0.083deg-1x2160x4320-1.0.2 "aerosols:dir=aerosol" -c cube-high-res.config
+bsub -W 12:00 -M 64000 -e ch4_l.err -o ch4_l.out -J ch4_l cube-gen esdc/ch4/esdc-8d-0.25deg-1x720x1440-1.0.2 "ch4:dir=CH4" -c cube-low-res.config
+bsub -W 12:00 -M 64000 -e ch4_h.err -o ch4_h.out -J ch4_h cube-gen esdc/ch4/esdc-8d-0.083deg-1x2160x4320-1.0.2 "ch4:dir=CH4" -c cube-high-res.config
+bsub -W 12:00 -M 64000 -e co2_l.err -o co2_l.out -J co2_l cube-gen esdc/co2/esdc-8d-0.25deg-1x720x1440-1.0.2 "co2:dir=CO2" -c cube-low-res.config
+bsub -W 12:00 -M 64000 -e co2_h.err -o co2_h.out -J co2_h cube-gen esdc/co2/esdc-8d-0.083deg-1x2160x4320-1.0.2 "co2:dir=CO2" -c cube-high-res.config
+bsub -W 12:00 -M 64000 -e ozone_temis_l.err -o ozone_temis_l.out -J ozone_temis_l cube-gen esdc/ozone_temis/esdc-8d-0.25deg-1x720x1440-1.0.2 "ozone_temis:dir=ozone_temis" -c cube-low-res.config
+bsub -W 12:00 -M 64000 -e ozone_temis_h.err -o ozone_temis_h.out -J ozone_temis_h cube-gen esdc/ozone_temis/esdc-8d-0.083deg-1x2160x4320-1.0.2 "ozone_temis:dir=ozone_temis" -c cube-high-res.config
+
+
+bsub -W 12:00 -M 64000 -e cloud_l.err -o cloud_l.out -J cloud_l cube-gen esdc/cloud/esdc-8d-0.25deg-1x720x1440-1.0.2 "cloud:dir=cloud" -c cube-low-res.config
+bsub -W 12:00 -M 64000 -e cloud_h.err -o cloud_h.out -J cloud_h cube-gen esdc/cloud/esdc-8d-0.083deg-1x2160x4320-1.0.2 "cloud:dir=cloud" -c cube-high-res.config
+
+bsub -W 12:00 -M 64000 -e soil_l.err -o soil_l.out -J soil_l cube-gen esdc/soil_moisture/esdc-8d-0.25deg-1x720x1440-1.0.2 "soil_moisture_esacci:dir=soil_moisture_tt" -c cube-low-res.config
+bsub -W 12:00 -M 64000 -e soil_h.err -o soil_h.out -J soil_h cube-gen esdc/soil_moisture/esdc-8d-0.083deg-1x2160x4320-1.0.2 "soil_moisture_esacci:dir=soil_moisture_tt" -c cube-high-res.config
+
+bsub -W 23:00 -M 64000 -e sst_l.err -o sst_l.out -J sst_l cube-gen esdc/sst/esdc-8d-0.25deg-1x720x1440-1.0.2 "sst:dir=sst" -c cube-low-res.config
+bsub -W 23:00 -M 64000 -e sst_h.err -o sst_h.out -J sst_h cube-gen esdc/sst/esdc-8d-0.083deg-1x2160x4320-1.0.2 "sst:dir=sst" -c cube-high-res.config
+
+bsub -W 23:00 -M 64000 -e oc_l.err -o oc_l.out -J oc_l cube-gen esdc/oc/esdc-8d-0.25deg-1x720x1440-1.0.2 "oc:dir=OC-daily" -c cube-low-res.config
+bsub -W 23:00 -M 64000 -e oc_h.err -o oc_h.out -J oc_h cube-gen esdc/oc/esdc-8d-0.083deg-1x2160x4320-1.0.2 "oc:dir=OC-daily" -c cube-high-res.config
+
 
 END_TIME=$(date)
 
